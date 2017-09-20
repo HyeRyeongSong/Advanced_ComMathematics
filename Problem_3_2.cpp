4ec5d25ed3c0eecb7f7b72399373987eee18ad82
= <=>
2017-09-20 21:51:36 +0900

    Homework #1 : Problem_3_2

--- Problem_3_2.cpp
+++ Problem_3_2.cpp
@@ -1,6 +1,7 @@
 //
 // Created by HyeRyeongSong on 2017. 9. 14..
 //
+
 #include <iostream>
 #include <cmath>
 
@@ -9,12 +10,12 @@ using namespace std;
 struct Term
 {
     double coef;   // coefficient
-    int exp; // exponent
+    double exp; // exponent
 
     void Integral()
     {
         this->coef = (this->coef) * (this->exp);
-        --(this->exp);
+        (this->exp)-=1;
         if(this->exp < 0) {
             this->coef = 0;
             this->exp = 0;
@@ -34,21 +35,20 @@ Term* IntegralPoly(Term* terms);
 
 int main()
 {
-    Term terms[4] = {{25, 3}, {-6, 2}, {7, 1}, {-88, 0}};
+    Term terms[5] = {{-0.1, 4}, {-0.15, 3}, {-0.5, 2}, {-0.25, 1}, {1.2, 0}};
 
-    double true_Value = CalculatePoly(terms, 3);
+    double true_Value = CalculatePoly(terms, 1);
 
     double Approximation = 0;
 
     double et;
 
-    int j=0;
 
-    for(j = 0; j<4; ++j)
+    for(int j = 0; j<5; ++j)
     {
-        Approximation += (CalculatePoly(terms, 1) * pow(3-1,j) / Factorial((double)j));
+        Approximation += (CalculatePoly(terms, 0) * pow(1-0,j) / Factorial((double)j));
         et = fabs(true_Value - Approximation) / true_Value * 100;
-        printf("j=%2d f(3)= %.3f et = %3.3f %% \n",j,Approximation,et);
+        printf("j=%2d f(1)= %.3f et = %3.3f %% \n",j,Approximation,et);
         IntegralPoly(terms);
     }
 
@@ -72,14 +72,14 @@ double CalculateMono(Term term, double value)
 double CalculatePoly(Term* terms, double value)
 {
     double sum = 0;
-    for(int i=0; i<4; ++i)
+    for(int i=0; i<5; ++i)
         sum+=CalculateMono(terms[i], value);
     return sum;
 }
 
 Term* IntegralPoly(Term* terms)
 {
-    for(int i=0; i<4; ++i)
+    for(int i=0; i<5; ++i)
         terms[i].Integral();
     return terms;
 }
--- cmake-build-debug/CMakeFiles/Com_Mathematics.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Com_Mathematics.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Com_Mathematics.dir/Problem_3_1.cpp.o"
+  "CMakeFiles/Com_Mathematics.dir/Problem_3_2.cpp.o"
   "Com_Mathematics.pdb"
   "Com_Mathematics"
 )
